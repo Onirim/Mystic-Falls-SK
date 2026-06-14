929a644bac6a3ddd3d64ffa049feb3b03abc19ac
Thibaut C. <20838877+Onirim@users.noreply.github.com>
2026-06-14 14:31:29 +0200

    Add files via upload

--- generage_pdf.cmd
+++ generage_pdf.cmd
@@ -1,3 +1,3 @@
-pandoc ".\Mystic Falls - Regles du jeu.md" -o ".\Mystic Falls.pdf" --pdf-engine=lualatex --toc -V papersize=a5 -V geometry:"top=1.5cm, bottom=2cm, inner=1.7cm, outer=1.5cm" -V fontsize=11pt -V mainfont="Georgia" -V lang=fr -V classoption=twoside -H style.tex
+pandoc ".\Les monstres que nous sommes - Regles du jeu.md" -o ".\Les monstres que nous sommes - Regles du jeu.pdf" --pdf-engine=lualatex --toc -V papersize=a5 -V geometry:"top=1.5cm, bottom=2cm, inner=1.7cm, outer=1.5cm" -V fontsize=11pt -V mainfont="Georgia" -V lang=fr -V classoption=twoside -H style.tex
 
 pause
